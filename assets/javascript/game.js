120c94b2d33dde192225e6fbb3025f13bb24539c
Aldo Sanchez <Aldo Sanchez>
2016-11-01 20:44:08 -0500

    select random word from array, and display empty slots

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,15 @@
 console.log("test");
+
+//words available for hangman game
+var wordArray = ["luisana","aldo","melquiades","coffee","black","hole","cellphone","test"];
+//selects random word from array, determines length of word, and displays empty slots for selected word
+var randomWord = Math.floor(Math.random()*wordArray.length);
+var lengthWord = wordArray[randomWord].length;
+var emptyWord = "";
+for (i = 0; i < lengthWord; i++){
+  emptyWord = emptyWord + " _";
+};
+
+console.log(wordArray[randomWord]);
+console.log(lengthWord);
+console.log(emptyWord);
