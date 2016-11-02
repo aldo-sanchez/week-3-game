afbedd023abf270408d5cb070743d4375d213855
Aldo Sanchez <Aldo Sanchez>
2016-11-02 18:33:01 -0500

    added userValidation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 console.log("test");
 
 //words available for hangman game
-// var wordArray = ["luisana","aldo","melquiades","coffee","black","hole","cellphone","test"];
-var wordArray = ["luisana"];
+ var wordArray = ["luisana","aldo","melquiades","coffee","black","hole","cellphone","test"];
+//var wordArray = ["luisana"];
 
 //selects a random word from array, determines length of word, and assigns a selected word to a variable (selectedWord).  
 var randomWord = Math.floor(Math.random()*wordArray.length);
@@ -11,10 +11,9 @@ var selectedWord = wordArray[randomWord];
 
 // initialize an emptyWord that will be compared to  selectedWord
 var emptyWord = "";
-var oldWord = "";
 
 // gameStatus object will keep track of game and is initialized here.
-var gameStatus = {isPlaying:true, numberWins:0, numberLosses:0, lives:6}
+var gameStatus = {isPlaying:false, numberWins:0, numberLosses:0, lives:6}
 console.log(wordArray[randomWord]);
 console.log(wordLength);
 
@@ -31,49 +30,65 @@ for (i = 0; i < wordLength; i++){
 console.log(selectedWordArray);
 console.log(emptyWord);
 
+console.log(gameStatus.isPlaying);
+$(document).ready(function () {
 
-$(document).ready(function(){
-  $(document).keypress(function userGuess(event){
-    var userInput = String.fromCharCode(event.keyCode).toLowerCase();
-    console.log(userInput);
-    emptyWord = "";
+    if (gameStatus.isPlaying = false) {
+        $(document).keypress(function startGame(event) {
+            userInput = event.keyCode
+            if (userInput = 32) {
+                return gameStatus.isPlaying = true;
+                console.log(gameStatus.isPlaying);
+            }
+        });
+    }
+    else if (gameStatus.isPlaying = true) {
+        $(document).keypress(function userGuess(event) {
+            var userInput = String.fromCharCode(event.keyCode).toLowerCase();
+            console.log(userInput);
+            wrongLetterWord = emptyWord;
+            emptyWord = "";
+            var isUserGuessValid = false;
+
+            //Compare selected word with user inputs.  If user input = selected word then the input replaces the empty slot in emptyWord.
+            for (i = 0; i < wordLength; i++) {
+                if (selectedWordArray[i] == userInput) {
+                    emptyWordArray[i] = userInput;
+                    isUserGuessValid = true;
+                }
+                emptyWord = emptyWord + emptyWordArray[i];
+            };
+
+            //will only enter if it isnot valid and doesnt already exist. 
+            if (!isUserGuessValid && wrongLetterArray.indexOf(userInput) == -1) {
+                gameStatus.lives--;
+                wrongLetterArray.push(userInput);
+                console.log(wrongLetterArray);
+            }
+
+            console.log(emptyWordArray);
+            console.log(wrongLetterArray);
+            console.log(gameStatus.lives);
+
+            function game() {
+                if (gameStatus.lives > 0 && emptyWord == selectedWord) {
+                    alert("you win :)");
+                    gameStatus.isPlaying = false;
+                    gameStatus.numberWins++;
+                }
+                else if (gameStatus.lives <= 0) {
+                    alert("you lose :(")
+                    gameStatus.isPlaying = false;
+                    gameStatus.numberLosses++;
+                };
+            };
+            game();
+
+        });
+
+    }
   
-    //Compare selected word with user inputs.  If user input = selected word then the input replaces the empty slot in emptyWord.
-    for (i = 0; i < wordLength; i++){
-      if (selectedWordArray[i] == userInput){
-        emptyWordArray[i] = userInput;
-      }
-      // else{
-      //   wrongLetterArray[i] = userInput;
-      // }
-      emptyWord = emptyWord + emptyWordArray[i];
-      // wrongLetterWord = wrongLetterArray + wrongLetterArray[i];
-    };
-    console.log(emptyWordArray);
-    console.log(emptyWord);    
-    // console.log(wrongLetterArray);
-    // if (emptyWord == selectedWord) {
-    //   numberWins++;
-    //   alert("you win!");
-    //   console.log(numberWins);
-    // }
-    // else if (lives = 0){
-    //   alert("you lose");
-    // }
-    $(document).keypress(function game(event){
-      if (gameStatus.lives > 0 && emptyWord == selectedWord){
-        alert("you win :)");
-        gameStatus.isPlaying = false;
-        gameStatus.numberWins++;
-        }
-      else if (gameStatus.lives <= 0){
-        alert("you lose :(")
-        gameStatus.isPlaying = false;
-        gameStatus.numberLosses++;
-      };
-    });
-    // console.log(gameStatus.lives);
-  });
+  
 });
 
 
