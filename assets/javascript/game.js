7f750d03ccac15acbe8354fa55c00b35ede8c9d1
Aldo Sanchez <Aldo Sanchez>
2016-11-01 22:16:46 -0500

    added user input for comparison

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,34 +5,39 @@ console.log("test");
 var wordArray = ["luisana"];
 //selects random word from array, determines length of word, and displays empty slots for selected word
 var randomWord = Math.floor(Math.random()*wordArray.length);
-var lengthWord = wordArray[randomWord].length;
+var wordLength = wordArray[randomWord].length;
 
-//may not use this bottom stuff
+//may not use this stuff
 // var emptyWord = "";
 // for (i = 0; i < lengthWord; i++){
 //   emptyWord = emptyWord + "_ ";
 // };
 
 console.log(wordArray[randomWord]);
-console.log(lengthWord);
+console.log(wordLength);
 // console.log(emptyWord);
 
 
 var selectedWordArray = [];
 var emptyWord = [];
-for (i = 0; i < lengthWord; i++){
+for (i = 0; i < wordLength; i++){
   selectedWordArray[i] = wordArray[randomWord].substr(i,1);
   emptyWord[i] = "_"
 };
 console.log(selectedWordArray);
 console.log(emptyWord);
 
-var userInput = "a";
-
 //Compare selected word with user inputs.  If user input = selected word then the input replaces the empty slot in emptyWord.
-for (i = 0; i < selectedWordArray.length; i++){
-  if (selectedWordArray[i] == userInput){
-    emptyWord[i] = userInput;
-  };
-};
-console.log(emptyWord);
+$(document).ready(function(){
+  $(document).keypress(function userGuess(event){
+    var userInput = String.fromCharCode(event.keyCode).toLowerCase();
+    console.log(userInput);
+    for (i = 0; i < wordLength; i++){
+      if (selectedWordArray[i] == userInput){
+        emptyWord[i] = userInput;
+      };
+    };
+    console.log(emptyWord);
+  });
+});
+
